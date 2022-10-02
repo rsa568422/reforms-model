f8274d84aac619e803306e126eadef487c164235
Roberto Sánchez Arévalo <rsa568422@hotmail.com>
2022-10-02 15:27:28 +0200

    Cambios menores

--- src/test/java/sa/reforms/entities/PriceTableJobTest.java
+++ src/test/java/sa/reforms/entities/PriceTableJobTest.java
@@ -2,6 +2,8 @@ package sa.reforms.entities;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import sa.reforms.entities.data.PriceTableJobData;
 import sa.reforms.exceptions.InvalidParamsException;
 
@@ -19,36 +21,11 @@ class PriceTableJobTest {
         this.priceTableJob = PriceTableJobData.PT_JOB_PAINTWORK_PLASTIC();
     }
 
-    @Test
-    void test_getPrize_quantity_3() {
-        Optional<Double> quantity = Optional.of(3.0);
-        BigDecimal expected = new BigDecimal("20.00");
-
-        BigDecimal actual = this.priceTableJob.getPrize(quantity);
-
-        assertAll(
-                () -> assertEquals(0, expected.compareTo(actual)),
-                () -> assertEquals(expected.toPlainString(), actual.toPlainString())
-        );
-    }
-
-    @Test
-    void test_getPrize_quantity_12() {
-        Optional<Double> quantity = Optional.of(12.0);
-        BigDecimal expected = new BigDecimal("54.00");
-
-        BigDecimal actual = this.priceTableJob.getPrize(quantity);
-
-        assertAll(
-                () -> assertEquals(0, expected.compareTo(actual)),
-                () -> assertEquals(expected.toPlainString(), actual.toPlainString())
-        );
-    }
-
-    @Test
-    void test_getPrize_quantity_50() {
-        Optional<Double> quantity = Optional.of(50.0);
-        BigDecimal expected = new BigDecimal("200.00");
+    @ParameterizedTest
+    @CsvSource({ "3.0, 20.00", "12.0, 54.00", "50.0, 200.00" })
+    void test_getPrize(String stringQuantity, String stringExpected) {
+        Optional<Double> quantity = Optional.of(Double.valueOf(stringQuantity));
+        BigDecimal expected = new BigDecimal(stringExpected);
 
         BigDecimal actual = this.priceTableJob.getPrize(quantity);
 
