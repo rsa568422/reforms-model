e87648beb4b4cf653721f3d7e16bf838c9acff4d
rsa568422 <rsa568422@gmail.com>
2022-10-03 21:03:01 +0200

    Limpieza en el proyecto tras fallo en merge.

--- src/test/java/sa/reforms/entities/data/ContractedJobData.java
+++
@@ -1,9 +0,0 @@
-package sa.reforms.entities.data;
-
-import sa.reforms.entities.Insurer;
-
-public class ContractedJobData extends JobData {
-
-    public static final Insurer INSURER_A = new Insurer("Insurer_A");
-
-}
