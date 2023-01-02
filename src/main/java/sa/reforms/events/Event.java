f84bb6b4d293a2a07cd24075dd6d01df74d2214a
Roberto Sánchez Arévalo <rsa568422@hotmail.com>
2023-01-02 14:13:53 +0100

    Se agregan comentarios a los eventos

--- src/main/java/sa/reforms/events/Event.java
+++ src/main/java/sa/reforms/events/Event.java
@@ -1,6 +1,7 @@
 package sa.reforms.events;
 
 import java.util.Date;
+import java.util.Optional;
 
 import lombok.*;
 
@@ -12,13 +13,25 @@ public class Event implements Comparable<Event> {
     @NonNull
     private final Date date;
 
+    private Optional<String> comments;
+
     public Event(@NonNull Date date) {
         this.date = date;
+        this.comments = Optional.empty();
+    }
+
+    public Event(@NonNull Date date, @NonNull String comments) {
+        this.date = date;
+        this.comments = Optional.of(comments);
     }
 
     @Override
     public int compareTo(Event event) {
         return this.date.compareTo(event.getDate());
+    }
+
+    public void setComments(@NonNull String comments) {
+        this.comments = Optional.of(comments);
     }
 
 }
